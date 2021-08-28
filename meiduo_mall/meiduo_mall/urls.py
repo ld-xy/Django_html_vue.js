330bceccc1004d98d34b9f7171fe8e64bab6d409
liudeng <1614030192@qq.com>
2021-08-28 15:27:45 +0800

    修改

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-import xadmin
+# import xadmin
 from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
-    url(r'xadmin/', include(xadmin.site.urls)),  # xadmin
+    url(r'^admin/', admin.site.urls),
+    # url(r'xadmin/', include(xadmin.site.urls)),  # xadmin
 
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),  # 富文本编辑器
 
